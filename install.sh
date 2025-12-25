7983a9742df50b80fa01fda5e127094538159805
Lipsjs <81687798+Lipsjs@users.noreply.github.com>
2025-12-26 01:50:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 set -e
 
-# Make sure ~/bin exists
-mkdir -p ~/bin
+# Ensure ~/bin exists
+mkdir -p "$HOME/bin"
 
-# Download the script from GitHub
-curl -L -o ~/bin/arch-maintainer https://raw.githubusercontent.com/YOUR_USERNAME/arch-maintainer/main/arch-tool
+# Download the raw script
+curl -L -o "$HOME/bin/arch-maintainer" \
+  https://raw.githubusercontent.com/Lipsjs/Arch_Maintainer/main/arch-maintainer
 
 # Make it executable
-chmod +x ~/bin/arch-maintainer
+chmod +x "$HOME/bin/arch-maintainer"
 
 echo "Installation complete!"
 echo "You can now run 'arch-maintainer' from anywhere."
